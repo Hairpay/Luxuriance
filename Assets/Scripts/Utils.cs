ab425e861ab53ce87b48e646c3978a3a6200e543
Aymeric <xhin_12@hotmail.fr>
2019-09-10 00:47:01 +0200

    script scene avatar states & movement

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Serializable]
+public class Utils
+{
+    public static bool IsFloatEpsilon(float value, float epsilon = 0.01f )
+    {
+        return value > -epsilon && value < epsilon;
+    }
+
+    public static bool IsFloatGreaterThanEpsilon(float value, float epsilon = 0.01f )
+    {
+        return value > epsilon;
+    }
+}
