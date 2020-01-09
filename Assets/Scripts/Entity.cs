3f2dff31ad4e3c86e9eb19adb6d72571d5fa3274
Aymeric <xhin_12@hotmail.fr>
2020-01-09 23:38:29 +0100

    working on crouch/crawl : all good except for the way to exit crouch/crawl mode

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -6,7 +6,11 @@ public abstract class Entity : MonoBehaviour
     public delegate void ExecuteState();
 
     public float _moveSpeed;
-    public float _collisionDetectionDistance;
+    public float _collisionHitDistance;
+    public float _collisionHitDistanceBottomLength = 1.0f;
+    public float _collisionHitDistanceTopLength = 1.0f;
+    public float _collisionHitDistanceLeftLength = 1.0f;
+    public float _collisionHitDistanceRightLength = 1.0f;
     public bool _drawCollisionsEnabled;
 
     protected class CollisionDirection
@@ -76,7 +80,7 @@ public abstract class Entity : MonoBehaviour
     private void Awake()
     {
         _collisionDirection = new CollisionDirection();
-        _collider = GetComponent<Collider2D>();
+        _collider = GetComponentInChildren<Collider2D>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _gravityScaleDefault = _rigidbody.gravityScale;
@@ -113,45 +117,45 @@ public abstract class Entity : MonoBehaviour
         Vector2 colliderBottomLeft = new Vector2( _collider.bounds.min.x, _collider.bounds.min.y - offset );
         Vector2 colliderBottomRight = new Vector2( _collider.bounds.min.x + _collider.bounds.size.x, _collider.bounds.min.y - offset );
 
-        _collisionDirection.Bottom = Physics2D.Raycast( colliderBottomLeft, Vector2.down, _collisionDetectionDistance )
-            || Physics2D.Raycast( colliderBottomRight, Vector2.down, _collisionDetectionDistance );
+        _collisionDirection.Bottom = Physics2D.Raycast( colliderBottomLeft, Vector2.down, _collisionHitDistance * _collisionHitDistanceBottomLength )
+            || Physics2D.Raycast( colliderBottomRight, Vector2.down, _collisionHitDistance * _collisionHitDistanceBottomLength );
         #endregion
 
         #region Collision: Top
         Vector2 colliderTopLeft = new Vector2( _collider.bounds.min.x, _collider.bounds.max.y + offset );
         Vector2 colliderTopRight = new Vector2( _collider.bounds.max.x, _collider.bounds.max.y + offset );
 
-        _collisionDirection.Top = Physics2D.Raycast( colliderTopLeft, Vector2.up, _collisionDetectionDistance )
-            || Physics2D.Raycast( colliderTopRight, Vector2.up, _collisionDetectionDistance );
+        _collisionDirection.Top = Physics2D.Raycast( colliderTopLeft, Vector2.up, _collisionHitDistance * _collisionHitDistanceTopLength )
+            || Physics2D.Raycast( colliderTopRight, Vector2.up, _collisionHitDistance * _collisionHitDistanceTopLength );
         #endregion
 
         #region Collision: Left
         Vector2 colliderLeftBottom = new Vector2( _collider.bounds.min.x - offset, _collider.bounds.min.y );
         Vector2 colliderLeftTop = new Vector2( _collider.bounds.min.x - offset, _collider.bounds.max.y );
 
-        _collisionDirection.LeftBottom = Physics2D.Raycast( colliderLeftBottom, Vector2.left, _collisionDetectionDistance );
-        _collisionDirection.LeftTop = Physics2D.Raycast( colliderLeftTop, Vector2.left, _collisionDetectionDistance );
+        _collisionDirection.LeftBottom = Physics2D.Raycast( colliderLeftBottom, Vector2.left, _collisionHitDistance * _collisionHitDistanceLeftLength );
+        _collisionDirection.LeftTop = Physics2D.Raycast( colliderLeftTop, Vector2.left, _collisionHitDistance * _collisionHitDistanceLeftLength );
         #endregion
 
         #region Collision: Right
         Vector2 colliderRightBottom = new Vector2( _collider.bounds.max.x + offset, _collider.bounds.min.y );
         Vector2 colliderRightTop = new Vector2( _collider.bounds.max.x + offset, _collider.bounds.max.y );
 
-        _collisionDirection.RightBottom = Physics2D.Raycast( colliderRightBottom, Vector2.right, _collisionDetectionDistance );
-        _collisionDirection.RightTop = Physics2D.Raycast( colliderRightTop, Vector2.right, _collisionDetectionDistance );
+        _collisionDirection.RightBottom = Physics2D.Raycast( colliderRightBottom, Vector2.right, _collisionHitDistance * _collisionHitDistanceRightLength );
+        _collisionDirection.RightTop = Physics2D.Raycast( colliderRightTop, Vector2.right, _collisionHitDistance * _collisionHitDistanceRightLength );
         #endregion
 
         #region Draw Rays
         if( _drawCollisionsEnabled )
         {
-            Debug.DrawRay( colliderBottomLeft, new Vector2( 0, -_collisionDetectionDistance ), Color.cyan );
-            Debug.DrawRay( colliderBottomRight, new Vector2( 0, -_collisionDetectionDistance ), Color.cyan );
-            Debug.DrawRay( colliderTopLeft, new Vector2( 0, _collisionDetectionDistance ), Color.cyan );
-            Debug.DrawRay( colliderTopRight, new Vector2( 0, _collisionDetectionDistance ), Color.cyan );
-            Debug.DrawRay( colliderLeftBottom, new Vector2( -_collisionDetectionDistance, 0 ), Color.cyan );
-            Debug.DrawRay( colliderLeftTop, new Vector2( -_collisionDetectionDistance, 0 ), Color.cyan );
-            Debug.DrawRay( colliderRightBottom, new Vector2( _collisionDetectionDistance, 0 ), Color.cyan );
-            Debug.DrawRay( colliderRightTop, new Vector2( _collisionDetectionDistance, 0 ), Color.cyan );
+            Debug.DrawRay( colliderBottomLeft, new Vector2( 0, -_collisionHitDistance * _collisionHitDistanceBottomLength ), Color.cyan );
+            Debug.DrawRay( colliderBottomRight, new Vector2( 0, -_collisionHitDistance * _collisionHitDistanceBottomLength ), Color.cyan );
+            Debug.DrawRay( colliderTopLeft, new Vector2( 0, _collisionHitDistance * _collisionHitDistanceTopLength ), Color.cyan );
+            Debug.DrawRay( colliderTopRight, new Vector2( 0, _collisionHitDistance * _collisionHitDistanceTopLength ), Color.cyan );
+            Debug.DrawRay( colliderLeftBottom, new Vector2( -_collisionHitDistance * _collisionHitDistanceLeftLength, 0 ), Color.cyan );
+            Debug.DrawRay( colliderLeftTop, new Vector2( -_collisionHitDistance * _collisionHitDistanceLeftLength, 0 ), Color.cyan );
+            Debug.DrawRay( colliderRightBottom, new Vector2( _collisionHitDistance * _collisionHitDistanceRightLength, 0 ), Color.cyan );
+            Debug.DrawRay( colliderRightTop, new Vector2( _collisionHitDistance * _collisionHitDistanceRightLength, 0 ), Color.cyan );
         }
         #endregion
     }
